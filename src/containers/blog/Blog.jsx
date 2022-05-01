5a4584bfbd98148c3340f0737931890e79eff638
AmilHus <46779076+AmilHus@users.noreply.github.com>
2022-05-02 02:35:46 +0400

    Add files via upload

---
+++ src/containers/blog/Blog.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import './blog.css';
+import { Article } from '../../components'
+import {blog1, blog2,blog3,blog4,blog5} from '../../containers/blog/imports'
+
+const Blog = () => {
+    return(
+        <div className="gpt3__blog section__padding" id="blog">
+            <div className="gpt3__blog-heading">
+                <h1 className="gradient__text">A lot is happening, We are blogging about it.</h1>
+            </div>
+            <div className="gpt3__blog-container">
+                <div className="gpt3__blog-container_groupA">
+                    <Article imgUrl={blog1} />
+                </div>
+                <div className="gpt3__blog-container_groupB">
+                    <Article imgUrl={blog2}  />
+                    <Article imgUrl={blog3}  />
+                    <Article imgUrl={blog4}  />
+                    <Article imgUrl={blog5}  />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Blog
